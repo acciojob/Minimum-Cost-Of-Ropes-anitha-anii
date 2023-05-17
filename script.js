dfede98dc6722eba84651fba8467c6fb475fe4f8
Anitha chowdary <130220172+anitha-anii@users.noreply.github.com>
2023-05-17 13:46:10 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,10 +1,11 @@
+let n = 5
+let arr[] = {4, 2, 7, 6, 9}
 function calculateMinCost() {
   //your code here
-	
   let sum=0;
-	for (let i = 0; i <arr.length ; i++) {
+	for(let i=0;i<arr.length;i++){
 		sum+=arr[i];
 	}
-  
   return sum;
-}  
+  
+}
