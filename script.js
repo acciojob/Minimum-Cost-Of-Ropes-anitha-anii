62716f1f85ba3e4fa509b142a2c891fd924cb09b
Anitha chowdary <130220172+anitha-anii@users.noreply.github.com>
2023-07-02 18:43:46 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,120 +1,38 @@
-// Function to calculate minimum cost of ropes
-function calculateMinimumCostRopes() {
-  // Get the input values from the text field
-  var input = document.getElementById("ropes-input").value;
-
-  // Split the input string into an array of rope lengths
-  var ropes = input.split(",").map(Number);
-
-  // Create a priority queue (min heap)
-  var priorityQueue = new MinHeap();
-
-  // Insert all ropes into the priority queue
-  for (var i = 0; i < ropes.length; i++) {
-    priorityQueue.insert(ropes[i]);
-  }
-
-  // Variable to store the total cost
-  var totalCost = 0;
-
-  // Merge ropes until only one rope is left in the priority queue
-  while (priorityQueue.size() > 1) {
-    // Extract the two smallest ropes from the priority queue
-    var rope1 = priorityQueue.extractMin();
-    var rope2 = priorityQueue.extractMin();
-
-    // Calculate the cost of merging the ropes
-    var cost = rope1 + rope2;
-
-    // Add the cost to the total cost
-    totalCost += cost;
-
-    // Insert the merged rope back into the priority queue
-    priorityQueue.insert(cost);
-  }
-
-  // Output the minimum cost of connecting the ropes
-  document.getElementById("result").innerHTML = totalCost;
-}
-
-// MinHeap class implementation
-class MinHeap {
-  constructor() {
-    this.heap = [];
-  }
-
-  size() {
-    return this.heap.length;
-  }
-
-  insert(value) {
-    this.heap.push(value);
-    this.heapifyUp(this.heap.length - 1);
-  }
-
-  extractMin() {
-    if (this.heap.length === 0) {
-      return null;
-    }
-
-    if (this.heap.length === 1) {
-      return this.heap.pop();
-    }
-
-    var minValue = this.heap[0];
-    this.heap[0] = this.heap.pop();
-    this.heapifyDown(0);
-    return minValue;
-  }
-
-  heapifyUp(index) {
-    var parentIndex = Math.floor((index - 1) / 2);
-
-    if (parentIndex >= 0 && this.heap[parentIndex] > this.heap[index]) {
-      this.swap(parentIndex, index);
-      this.heapifyUp(parentIndex);
-    }
-  }
-
-  heapifyDown(index) {
-    var leftChildIndex = 2 * index + 1;
-    var rightChildIndex = 2 * index + 2;
-    var smallestIndex = index;
-
-    if (
-      leftChildIndex < this.heap.length &&
-      this.heap[leftChildIndex] < this.heap[smallestIndex]
-    ) {
-      smallestIndex = leftChildIndex;
-    }
-
-    if (
-      rightChildIndex < this.heap.length &&
-      this.heap[rightChildIndex] < this.heap[smallestIndex]
-    ) {
-      smallestIndex = rightChildIndex;
-    }
-
-    if (smallestIndex !== index) {
-      this.swap(smallestIndex, index);
-      this.heapifyDown(smallestIndex);
-    }
-  }
-
-  swap(index1, index2) {
-    var temp = this.heap[index1];
-    this.heap[index1] = this.heap[index2];
-    this.heap[index2] = temp;
-  }
-}
-
-// Call the function to calculate minimum cost when the form is submitted
-document.getElementById("ropes-form").addEventListener("submit", function (event) {
-  event.preventDefault();
-  calculateMinimumCostRopes();
-});
-
-
+function threeSum(arr, target) {
+// write your code here
+	arr.sort((a, b) => a - b);
   
+  // Initialize variables
+  let closestSum = arr[0] + arr[1] + arr[2];
+  
+  // Iterate through the array
+  for (let i = 0; i < arr.length - 2; i++) {
+    let left = i + 1;
+    let right = arr.length - 1;
+    
+    while (left < right) {
+      const sum = arr[i] + arr[left] + arr[right];
+      
+      // Check if the current sum is closer to the target
+      if (Math.abs(sum - target) < Math.abs(closestSum - target)) {
+        closestSum = sum;
+      }
+      
+      if (sum < target) {
+        left++;
+      } else if (sum > target) {
+        right--;
+      } else {
+        // Found an exact match, so return the sum
+        return sum;
+      }
+    }
+  }
+  
+  return closestSum;
+  
+}
+ 
+module.exports = threeSum;
     
   
